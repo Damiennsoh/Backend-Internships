d7e8aa514a6a3293f0ffabb66c1a1b6ce7796aaf
Damiennsoh <psycaro20@gmail.com>
2023-09-10 22:16:27 +0530

    created a new file named endpoint.py

---
+++ endpoint_task/endpoint.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask import request, jsonify
+from datetime import datetime
+
+
+app = Flask(__name__)
+@app.route('/api')
+def get_info():
+    # Get the value of the 'slack_name' parameter from the request URL
+    slack_name = request.args.get('Damien Ayine')
+    
+    # Get the value of the 'track' parameter from the request URL
+    track = request.args.get('backend')
+    
+    # Get the current day using the datetime module and format it as a string
+    current_day = datetime.now().strftime('%A')
+    
+    # Get the current UTC time using the datetime module and format it as a string
+    utc_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    # Create a dictionary containing the information to be returned as JSON
+    info = {
+        'slack_name': slack_name,
+        'current_day': current_day,
+        'utc_time': utc_time,
+        'track': track,
+        'github_file_url': 'https://github.com/username/repo/blob/main/file_name.ext',
+        'github_repo_url': 'https://github.com/Damiennsoh/Backend-Internships',
+        'status_code': 'success'
+    }
+
+    # Return the information as JSON response
+    return jsonify(info)
