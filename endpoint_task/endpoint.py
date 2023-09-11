beae0b9720c9ee26b7bb862a576fe2918d4e343a
Damiennsoh <psycaro20@gmail.com>
2023-09-11 12:43:47 +0530

    modified the endpoin.py file

--- endpoint_task/endpoint.py
+++ endpoint_task/endpoint.py
@@ -1,37 +1,39 @@
-from flask import Flask
-from flask import request, jsonify
+# Importing necessary dependencies/libraries
+from flask import Flask, jsonify, request
 from datetime import datetime
 
-
 app = Flask(__name__)
-@app.route('/api')
-def get_info():
-    # Get the value of the 'slack_name' parameter from the request URL
-    slack_name = request.args.get('Damien_Ayine')
-    
-    # Get the value of the 'track' parameter from the request URL
-    track = request.args.get('backend')
-    
-    # Get the current day using the datetime module and format it as a string
-    current_day = datetime.now().strftime('%A')
-    
-    # Get the current UTC time using the datetime module and format it as a string
-    utc_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
 
-    # Create a dictionary containing the information to be returned as JSON
-    info = {
+@app.route('/api', methods=['GET'])
+def get_info():
+    # Retrieve the values of 'slack_name' and 'track' from the query parameters
+    slack_name = request.args.get('slack_name', default='Damien_Ayine')
+    track = request.args.get('track', default='backend')
+
+    # Get the current day and time
+    current_day = datetime.now().strftime('%A')
+    current_time = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+
+    # Define the URLs for the GitHub file and repository
+    github_file_url = 'https://github.com/Damiennsoh/Backend-Internships/blob/main/endpoint_task/endpoint.py'
+    github_repo_url = 'https://github.com/Damiennsoh/Backend-Internships'
+
+    # Create a response dictionary with the retrieved values and URLs
+    response = {
         'slack_name': slack_name,
         'current_day': current_day,
-        'utc_time': utc_time,
+        'current_time': current_time,
         'track': track,
-        'github_file_url': 'https://github.com/Damiennsoh/Backend-Internships/blob/main/endpoint_task/endpoint.py',
-        'github_repo_url': 'https://github.com/Damiennsoh/Backend-Internships',
-        'status_code': 'success'
+        'github_file_url': github_file_url,
+        'github_repo_url': github_repo_url,
+        'status_code': 200
     }
 
-    # Return the information as JSON response
-    return jsonify(info)
+    # Return the response as JSON with a status code of 200
+    return jsonify(response), 200
+
 if __name__ == '__main__':
     app.run()
+
 
 
