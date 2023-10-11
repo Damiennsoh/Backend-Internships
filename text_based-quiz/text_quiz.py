afd4ca38fbc143a6d2f76e531d6f72d4839a77e2
Damiennsoh <psycaro20@gmail.com>
2023-10-11 11:11:16 +0530

    updated the text_quiz.py file with more lines of code

---
+++ text_based-quiz/text_quiz.py
@@ -0,0 +1,32 @@
+# Define the questions and choices
+questions = [
+    {"question": "What is the name of the capital city of France?",
+     "choices": ["A. Paris", "B. London", "C. Rome", "D. Rome"],
+    #  The correct answer
+     "answer": "A"
+    },
+    {"question": "Which planet is known as the red planet?",
+     "choices": ["A: Mars", "B: Venus", "C: Pluto", "D: Jupiter"],
+    #  The correct answer
+    "answer": "A"
+    },
+
+    ]
+
+# Create a function to present the questions and collect user answers.
+def run_quiz(questions):
+    score = 0
+    for question in questions:
+        print(question["question"])
+        for choice in question["choices"]:
+            print(choice)
+        answer = input("Enter your answer (A, B, C, or D): ")
+        if answer.upper() == question["answer"]:
+            score += 1
+    return score
+
+# Calculate the score based on the user's answers.
+quiz_score = run_quiz(questions)
+
+# Display results of ansers
+print("Your score is:", quiz_score)
